2a0a006114399b864a0d6209aeeeff8fb7f496d0
nsanzimfura eric <erickykress1@gmail.com>
2023-03-04 18:24:39 +0200

    postgress uncommenting

--- index.js
+++ index.js
@@ -21,30 +21,30 @@ app.use(passport.session());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
-// const sequelize = new Sequelize({
-//   dialect: "postgres",
-//   host: process.env.DATABASE_HOST,
-//   port: process.env.DATABASE_PORT,
-//   username: process.env.DATABASE_USERNAME,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: process.env.DATABASE_NAME,
-// });
+const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
 
-// const User = sequelize.define("user", {
-//   email: { type: Sequelize.STRING, allowNull: false, primaryKey: true },
-//   name: { type: Sequelize.STRING, allowNull: false },
-// });
+const User = sequelize.define("user", {
+  email: { type: Sequelize.STRING, allowNull: false, primaryKey: true },
+  name: { type: Sequelize.STRING, allowNull: false },
+});
 
-// User.sync();
+User.sync();
 
-// const SentEmail = sequelize.define("sent_email", {
-//   sender: { type: Sequelize.STRING, allowNull: false },
-//   recipients: { type: Sequelize.ARRAY(Sequelize.STRING), allowNull: false },
-//   subject: { type: Sequelize.STRING, allowNull: true },
-//   body: { type: Sequelize.STRING, allowNull: true },
-// });
+const SentEmail = sequelize.define("sent_email", {
+  sender: { type: Sequelize.STRING, allowNull: false },
+  recipients: { type: Sequelize.ARRAY(Sequelize.STRING), allowNull: false },
+  subject: { type: Sequelize.STRING, allowNull: true },
+  body: { type: Sequelize.STRING, allowNull: true },
+});
 
-// SentEmail.sync();
+SentEmail.sync();
 
 function isLoggedIn(req, res, next) {
   req.user ? next() : res.redirect("/auth/google");
